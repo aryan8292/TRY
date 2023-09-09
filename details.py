4f2c9e94797c570d09f2a8124ada9e17ae01e788
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2023-09-09 07:42:32 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,7 +1,7 @@
 api_id = 26600589
 api_hash = "1e06c253a61c1f4f548cbb9ebc09644d"
 bot_token = "6427925906:AAG_B7bPB7APrPFKKXcRBQDdRvtdIIdZ0ww"
-auth_users = [5079629749,-1001517345433]
+auth_users = [5079629749,-1001982028284]
 sudo_user = 5079629749
 log_channel = -1001517345433
 txt_channel = -1001517345433
